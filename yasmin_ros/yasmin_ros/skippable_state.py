348426ff2f95d1f4399b330b19c8a9bd586bd26f
PannapatC <PannapatC@arv.co.th>
2024-05-15 03:19:46 +0000

    add publisher

--- yasmin_ros/yasmin_ros/skippable_state.py
+++ yasmin_ros/yasmin_ros/skippable_state.py
@@ -1,14 +1,15 @@
-from typing import List, Callable, Type
-from std_srvs.srv import SetBool, Trigger
-
+from std_msgs.msg import String
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from typing import List, Callable, Type, Any
+ 
 from yasmin import State
 from yasmin import Blackboard
 from simple_node import Node
 from .basic_outcomes import SKIPPED, ABORT, SUCCEED, WAITING
-
-
+ 
+ 
 class SkippableState(State):
-
+ 
     def __init__(
         self,
         node: Node,
@@ -16,21 +17,28 @@ class SkippableState(State):
         execute_handler: Callable = None,
         srv_type: Type = Trigger,
         outcomes: List[str] = None,
+        pub_topic_name: str = None
     ) -> None:
-
+ 
         _outcomes = [SKIPPED, ABORT, SUCCEED]
-
+ 
         if outcomes:
             _outcomes = list(set(_outcomes + outcomes))
-
+ 
         self._skipped = False
         self.__srv = node.create_service(
             srv_type, skip_srv_name, self._skip_state)
-
+ 
         self.__execute_handler = execute_handler
-
+ 
+        if pub_topic_name:
+            pubsub_callback_group = MutuallyExclusiveCallbackGroup()
+            self.__pub_topic = node.create_publisher(String, pub_topic_name, 10,
+                                                      callback_group=pubsub_callback_group)
+        else: self.__pub_topic = None
+ 
         super().__init__(_outcomes)
-
+ 
     def _skip_state(
         self,
         req: Trigger.Request,
@@ -41,12 +49,12 @@ class SkippableState(State):
     
     def execute(self, blackboard: Blackboard) -> str:
         while True:
-            if self.__execute_handler: 
+            if self.__execute_handler:
                 outcome = self.__execute_handler(blackboard)
             if self._skipped:
                 self._skipped = False
                 return SKIPPED
-            if self._is_canceled(): 
+            if self._is_canceled():
                 return ABORT
             if outcome != WAITING: break
         return outcome
@@ -56,3 +64,14 @@ class SkippableState(State):
             self._canceled = False
             return True
         return False
+    
+    def publish_msg(self, msg: str):
+        """
+        Args:
+            msg: message (string) to publish
+        Raises:
+            ros2 publish message to <pub_topic_name>
+        """
+        _msg = String()
+        _msg.data = msg
+        if self.__pub_topic: self.__pub_topic.publish(_msg)
